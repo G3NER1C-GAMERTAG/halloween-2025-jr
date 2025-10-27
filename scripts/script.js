74c1cd6160670ae84a9e330271a30a900e7b83c8
G3NER1C-GAMERTAG <CTE-Ethan.King@sllboces.org>
2025-10-27 17:57:18 +0000

    updated halloween website

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// Wait for document to be fully loaded //
+$(document).ready(function() {
+
+    // ===========================
+    // FLOATING ICONS ANIMATION //
+    // ===========================
+
+    function animateFloatingIcons() {
+        $('.bat').animate({
+           top: '+=50px',
+           left: '+=30px'
+        }, 3000, function() {
+            $(this).animate({
+                top: '-=50px',
+                left: '-=30px'
+            }, 3000, animateFloatingIcons);
+            })
+        });
+        
+        $('.stars').animate({
+           top: '+=40px',
+           left: '+=40px',
+           opacity: 0.8
+        }, 2500, function() {
+            $(this).animate({
+                top: '-=40px',
+                left: '-=40px',
+                opacity: 0.6
+            }, 2500);
+        });
+    }
+})
